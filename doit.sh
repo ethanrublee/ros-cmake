9fc190eec7c71e3c4e1d2d3a84560715e8e8cccf
Troy Straszheim <straszheim@willowgarage.com>
2010-12-09 17:15:16 -0800

    bing

--- doit.sh
+++ doit.sh
@@ -3,5 +3,12 @@
 rm -f ./ros-cmake.rosinstall
 wget --no-check-certificate https://github.com/straszheim/ros-cmake/raw/master/ros-cmake.rosinstall
 
-rosinstall -n ri ./ros-cmake.rosinstall
+rosinstall -n ri/src ./ros-cmake.rosinstall
+cd ri/src
+
+. ./setup.sh
+./cmake/convert.py
+
+
+
 
