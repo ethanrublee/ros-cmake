520ef919985a54025d2bbdc2971698abf5a64965
Troy Straszheim <straszheim@willowgarage.com>
2010-12-15 19:46:00 -0800

    all work and no play makes johnny a dull boy

--- main.cmake
+++ main.cmake
@@ -1,17 +1,22 @@
 message(STATUS "--- main.cmake ---")
+
+#
+# Globally unique targets.  Wtf. 
+#
 cmake_policy(SET CMP0002 OLD)
 
 macro(rosbuild_3rdparty PKGNAME DEPFILE)
 
-  configure_file(install.sh.in ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/install.sh
-    @ONLY)
+  if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${DEPFILE})
+    configure_file(install.sh.in ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/install.sh
+      @ONLY)
   
-  execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/install.sh)
+    execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/install.sh)
+  endif()
 
 endmacro()
 
 add_custom_target(test-results-run)
-add_custom_target(3rdparty)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/private.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/public.cmake)
