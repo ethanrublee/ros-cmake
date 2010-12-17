18ddd4ad982bdbe4ee7c4db7a915f2cbc944128d
Troy Straszheim <straszheim@willowgarage.com>
2010-12-16 16:41:08 -0800

    bing

--- main.cmake
+++ main.cmake
@@ -6,6 +6,7 @@ set(ROSBUILD_SUBSHELL ${CMAKE_CURRENT_BINARY_DIR}/env.sh)
 set(ROS_PACKAGE_PATH $ENV{ROS_PACKAGE_PATH})
 set(ROSBUILD_GEN_DIR ${CMAKE_CURRENT_BINARY_DIR}/gen)
 file(MAKE_DIRECTORY ${ROSBUILD_GEN_DIR})
+include_directories(${ROSBUILD_GEN_DIR}/cpp/msg)
 
 #
 # Globally unique targets.  Wtf. 
