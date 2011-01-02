d13ba64559b28e94c009f367fe1060c6b8e71c4e
Troy Straszheim <straszheim@willowgarage.com>
2011-01-02 01:15:01 -0800

    tweak

--- fromscratch-rosmake.sh
+++ fromscratch-rosmake.sh
@@ -5,7 +5,7 @@ rm -rf scratch
 rm -f ros-rosmake.rosinstall
 wget --no-check-certificate https://github.com/straszheim/ros-cmake/raw/master/ros-rosmake.rosinstall
 
-rosinstall work-rosmake ros-cmake.rosinstall
+rosinstall work-rosmake ros-rosmake.rosinstall
 cd work-rosmake
 
 . ./setup.sh
