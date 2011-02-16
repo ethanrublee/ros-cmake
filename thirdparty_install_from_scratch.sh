2a4363c0cce5f215ad34465bfb0b770276f775e5
Troy Straszheim <straszheim@willowgarage.com>
2011-02-16 10:11:24 -0800

    checkpoint... fixing build

--- thirdparty_install_from_scratch.sh
+++ thirdparty_install_from_scratch.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -ex
 
-THIRDPARTYDIR=/opt/ros/unstable/3rdparty
+THIRDPARTYDIR=$1
 
-if [ ! -d $THIRDPARTY_DIR ] ; then
+if [ ! -d $THIRDPARTYDIR ] ; then
     sudo mkdir $THIRDPARTYDIR
 else
     sudo rm -rf $THIRDPARTYDIR/*
 fi
 sudo chown `whoami` $THIRDPARTYDIR
 
-git clone git://github.com/straszheim/ros-3rdparty.git
-cd ros-3rdparty
-make
+git clone git://github.com/straszheim/ros-3rdparty.git $THIRDPARTYDIR/src
+cd $THIRDPARTYDIR/src
+make INSTALL_PREFIX=$THIRDPARTYDIR
+
 
