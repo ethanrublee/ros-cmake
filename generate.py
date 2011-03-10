7f3048ec015260cc5c314543c890f1ea68ced09b
Troy Straszheim <straszheim@willowgarage.com>
2011-03-10 14:42:53 -0800

    making this work on rosbuild2 xml manifests

--- generate.py
+++ generate.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
-import os, os.path, sys, pprint, pickle, glob, em, yaml, rosbuild2
+import os, os.path, sys, pprint, pickle, glob, em, yaml, rosbuild2, pprint
+from StringIO import StringIO
+from xml.etree import *
+import xml.etree.ElementTree
 
 print "\nGenerating cmakelists in", sys.argv[1]
 
-MANIFEST="rosbuild.manifest"
+MANIFEST="manifest.xml"
 
 if len(sys.argv) == 1:
     print "usage: %s <ros_package_path>"
     sys.exit(1)
+
+okaypkgs = ['xmlrpcpp', 'cpp_common', 'rostime', 'roscpp_traits', 'roscpp_serialization']
+
 
 def get_package_dirs(p):
     pkgs = []
     def visit(arg, dirname, names):
         if MANIFEST in names:
             names[:] = [] # stop recursion
-            if dirname not in rosbuild2.thirdparty_projects + rosbuild2.broken_projects:
+            if os.path.basename(dirname) in okaypkgs:
                 arg += [dirname]
             else:
                 print "Skipping", dirname
@@ -33,34 +39,43 @@ for path in pkgpath:
 
 index = {}
 for pkgdir in pkgdirs:
-    d = yaml.load(open(pkgdir + "/" + MANIFEST))
-    d['srcdir'] = pkgdir
-    index[os.path.basename(pkgdir)] = d
-    
+    txt = open(pkgdir + "/" + MANIFEST).read()
+    d = xml.etree.ElementTree.fromstring(txt)
+    rb2 = d.find('rosbuild2')
+    if rb2:
+        rb2.set('srcdir', pkgdir)
+        bn = os.path.basename(pkgdir)
+        index[bn] = rb2
+        #print bn, "RB2=", rb2
+
 # generate 'recursive' dependencies 
 def get_recursive_depends(index, pkgname, stack=[]):
     if pkgname not in index:
         raise Exception("Uh oh, can't find %s in index to calculate dependencies.  stack=%s" % (pkgname, stack))
     v = index[pkgname]
-    # print v
+    print pkgname
     rdep = set([])
-    if 'depend' not in v:
-        return rdep
-    for dep in v['depend']:
-        # print ">>>", dep
-        rdep.add(dep)
-        rdep.update(get_recursive_depends(index, dep, stack + [pkgname]))
+    for dep in v.findall('depend'):
+        print pkgname, ">>>", dep
+        _ = dep.attrib['package']
+        #print ">!>!", _
+        rdep.add(_)
+        rdep.update(get_recursive_depends(index, _, stack + [pkgname]))
+    #print pkgname, "rdep=", rdep
     return rdep
 
 for k in index:
     rdep = get_recursive_depends(index, k)
-    index[k]['recursive_depends'] = rdep
-    
+    print "rd=", rdep
+    index[k].attrib['recursive_depends'] = rdep
+    #print "ttr:", k, index[k].attrib['recursive_depends']
 langs = {}
 
 for k, v in index.iteritems():
-    if 'export' in v and 'roslang' in v['export']:
-        langs[k] = v['export']['roslang']['cmake']
+    #print "VEEE:", v
+    for l in v.findall('export/roslang'):
+        #print "WOO:", l.attrib
+        langs[k] = l.attrib['cmake']
 
 #pprint.pprint(index)
 #print "langs=", langs
@@ -83,7 +98,7 @@ topo_pkgs = []
 
 def write_project_cmake(name, d, index=index):
     global topo_pkgs
-    print ">>>", name, '                    \r',
+    # print ">>>", name, '                    \r',
     sys.stdout.flush()
     bindir = sys.argv[3] + '/' + name
     if not os.path.isdir(bindir):
@@ -93,11 +108,9 @@ def write_project_cmake(name, d, index=index):
     pkgdict['brief_doc'] = d.get('brief', "no brief description")
     pkgdict['description'] = d.get('description', "no description")
 
-    if 'depend' in d:
-        pkgdict['DEPENDED_PACKAGE_PATHS'] = [index[pkgname]['srcdir']
-                                             for pkgname in d['depend']]
-    else:
-        pkgdict['DEPENDED_PACKAGE_PATHS'] = []
+    pkgdict['DEPENDED_PACKAGE_PATHS'] = [index[pkgname].attrib['srcdir']
+                                         for pkgname in 
+                                         [x.attrib['package'] for x in d.findall('depend')]]
 
     pkgdict['GENERATED_ACTIONS'] = d.get('actions', [])
     
@@ -105,12 +118,10 @@ def write_project_cmake(name, d, index=index):
     pkgdict['srvs'] = d.get('srvs', [])
     pkgdict['cfgs'] = d.get('cfgs', [])
     pkgdict['thirdparty'] = d['3rdparty'] if '3rdparty' in d else []
+    pkgdict['srcdir'] = d.attrib['srcdir']
 
-    pkgdict['exported_include_dirs'] = []
-
-    if 'export' in d:
-        if 'include_dirs' in d['export']:
-            pkgdict['exported_include_dirs'] = d['export']['include_dirs']
+    pkgdict['exported_include_dirs'] = [x.text for x in 
+                                        d.findall('export/include_dir')]
 
     libs_i_need = pkgdict['libs_i_need'] = []
     includes_i_need = pkgdict['includes_i_need'] = []
@@ -122,16 +133,26 @@ def write_project_cmake(name, d, index=index):
     pkgdict['config_definitions'] = d.get('export', {}).get('defines', [])
 
     pkgdict['depend'] = d.get('depend', [])
-    assert 'recursive_depends' in d
-    for pkgname in d['recursive_depends']:
+    #print ">>", d.attrib
+    assert 'recursive_depends' in d.attrib
+    print "RECDEPS:", name, "->", d.attrib['recursive_depends']
+    for pkgname in d.attrib['recursive_depends']:
+        #print "CHECKDEP", pkgname
         pkg = index[pkgname]
-        pkgcomment = [r'# %s' % pkgname]
-        if 'export' in pkg:
-            if 'include_dirs' in pkg['export']:
-                includes_i_need += pkg['export']['include_dirs']
+        pkgcomment = r'  # %s' % pkgname
+        for l in pkg.findall('export/lib'):
+            libs_i_need += [l.text + pkgcomment]
 
-            if 'libs' in pkg['export']:
-                libs_i_need += pkgcomment + pkg['export']['libs']
+        for i in pkg.findall('export/include_dir'):
+            includes_i_need += [i.text + "  # " + pkgname]
+
+        for d in pkg.findall('export/define'):
+            defines += [d.text + "  # " + pkgname]
+
+            
+        if 'export' in pkg:
+            #if 'include_dirs' in pkg['export']:
+            #    includes_i_need += pkg['export']['include_dirs']
 
             if 'defines' in pkg['export']:
                 defines += pkg['export']['defines']
@@ -139,7 +160,7 @@ def write_project_cmake(name, d, index=index):
             if 'swig' in pkg['export']:
                 pkgdict['swig_flags'] += pkgcomment + pkg['export']['swig']['flags']
                 
-    pkgdict['recursive_depends'] = d['recursive_depends']
+    pkgdict['recursive_depends'] = d.attrib['recursive_depends']
 
     pkgdict['defines'] = ['-D'+x for x in defines]
 
@@ -147,8 +168,10 @@ def write_project_cmake(name, d, index=index):
 
     pkgdict['pythondirs'] = d.get('pythondirs', [])
 
-#    print >>ofile, 'install(DIRECTORY %s DESTINATION share COMPONENT %s PATTERN ".svn" EXCLUDE REGEX ".*\\.(launch|xml|yaml|dox|srv|msg|cmake")' \
-    #% (d['srcdir'], name)
+
+    if name == "rostime":
+        print "PKGDICT:"
+        pprint.pprint(pkgdict)
 
     ofile = open(bindir + '/package.cmake', 'w')
     print >>ofile, em.expand(package_em, pkgdict)
@@ -211,6 +234,8 @@ d = dict(packages=index,
          topo_pkgs=topo_pkgs)
 
 print "Writing toplevel...."
+# pprint.pprint(d)
+
 toplevel_out.write(em.expand(toplevel_em, d))
 
 
